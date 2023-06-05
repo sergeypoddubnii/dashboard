c26136aa257be7e7164917bec4b10b4bd5c153b6
sergey poddubnii <poddubnii.sergey@gmail.com>
2023-06-05 22:11:30 +0300

    add registration login mock pages

---
+++ src/app/registration/page.tsx
@@ -0,0 +1,11 @@
+import React, {FC} from 'react';
+
+const Registration:FC = () => {
+    return (
+        <div>
+            Registration
+        </div>
+    );
+};
+
+export default Registration;
